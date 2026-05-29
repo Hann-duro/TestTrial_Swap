011ab1a3c1438066011452432d9c31d4d27ea7d2
Hann-duro <raichu4585@gmail.com>
2026-05-29 12:49:36 +0700

    character-feature

---
+++ Assets/PlayerFormController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerFormController : MonoBehaviour
+{
+    public CharacterData heroData;
+    public CharacterData wizardData;
+    public CharacterData rogueData;
+
+    public CharacterData currentCharacter;
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        ChangeForm(heroData);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ChangeForm(heroData);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            ChangeForm(wizardData);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            ChangeForm(rogueData);
+    }
+
+    public void ChangeForm(CharacterData newCharacter)
+    {
+        currentCharacter = newCharacter;
+
+        sr.sprite = currentCharacter.characterSprite;
+
+        Debug.Log("Changed to: " + currentCharacter.characterType);
+    }
+}
