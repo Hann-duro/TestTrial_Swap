b8c7175dfdd94e9de87d80c38922d6222ce8c85f
Hann-duro <raichu4585@gmail.com>
2026-05-31 14:38:42 +0700

    update monster&rogue

---
+++ Assets/CharacterData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "SwapScape/Character Data")]
+public class CharacterData : ScriptableObject
+{
+    public CharacterType characterType;
+
+    public Sprite characterSprite;
+
+    [Header("Stats")]
+    public int maxHealth = 1;
+
+    [Header("Abilities")]
+    public bool canPassSpike;
+
+    [TextArea]
+    public string abilityDescription;
+}
